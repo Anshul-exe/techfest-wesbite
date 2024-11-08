3dfb9c2a2945e744caa1d8912fb911275196df90
Arnav-lunatic <arnav4c@gmail.com>
2024-11-08 23:09:18 +0530

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        Hello world!
+      </h1>
     </>
   )
 }
