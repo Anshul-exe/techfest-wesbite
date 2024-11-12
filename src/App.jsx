ffc65d46abbc6592d8917ba1a265a8e907dcfbfb
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-11-13 01:50:27 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Glitch from './components/Home'
+import GlitchHomepage from './components/Home'
 import NavBar from './components/NavBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Glitch><NavBar/></Glitch>  
+    <GlitchHomepage/>
     </>
   )
 }
