8e00af1f0202a0b996b106216d90684df635e13b
shashank <shashankshekhar8534@gmail.com>
2025-02-05 20:52:02 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout";
-<<<<<<< HEAD
 import VenomSymbioteEffect from "./components/Venom";
 import GlitchHomepage from "./components/Home";
 import EyeOfAgamotto from "./components/EyeOfAgamotto";
 import AboutSection from "./components/Aboutus";
-=======
 // import InfinityGauntlet from "./components/Gauntlet";
 
->>>>>>> be71618f699621b6beb3bb187cd9061407bc5e5d
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-<<<<<<< HEAD
   <Layout />
     {/* <VenomSymbioteEffect/> */}
   </>
 );
-=======
-      <Layout />
-    </>
-  );
->>>>>>> be71618f699621b6beb3bb187cd9061407bc5e5d
 }
 
 export default App;
